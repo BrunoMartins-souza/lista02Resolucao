220fff1efe61abcbada8781952f21698abf07a42
BrunoMartins-souza <85463348+BrunoMartins-souza@users.noreply.github.com>
2024-09-18 16:20:12 -0300

    Resolução dos exercícios da lista 02 passada no dia 17/09/2024 pelo professor Fabricio

---
+++ src/pacote1/Main.java
@@ -0,0 +1,15 @@
+package pacote1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        int opc;
+
+        System.out.print("Escolha uma opção: ");
+        opc = input.nextInt();
+        
+    }
+}
