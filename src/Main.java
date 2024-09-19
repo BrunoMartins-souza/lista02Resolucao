1d1a8169de6d47d3cdff9d0e3ff769661ba2f8a7
Bruno F Martins <brunodefatimoms@gmail.com>
2024-09-18 21:55:52 -0300

    Resolvendo o exercicio 05

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import pacote1.Exercicio01;
 import pacote1.Exercicio02;
 import pacote1.Exercicio03;
 import pacote1.Exercicio04;
+import pacote1.Exercicio05;
 
 public class Main {
     public static void main(String[] args) {
@@ -30,6 +31,10 @@ public class Main {
                 case 4:
                     Exercicio04 e04 = new Exercicio04();
                     e04.exercicio04();
+                break;
+                case 5:
+                    Exercicio05 e05 = new Exercicio05();
+                    e05.exercicio05();
                 break;
 
 
